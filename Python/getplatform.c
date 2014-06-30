73fb2d20dece22e5f7c5cc3aae25af66359f48ae
miguel.catalan <miguel.catalan@pi2p.net>
2014-06-30 11:33:55 +0200

    new branch for android

---
+++ Python/getplatform.c
@@ -0,0 +1,38 @@
+#include "Python.h"
+
+#ifdef ANDROID
+#include <sys/utsname.h>
+#include <string.h>
+
+char _PLATFORM[20]={0}; // taken as base linux-armv7l-2.6
+
+const char *
+Py_GetPlatform(void)
+{
+    if (_PLATFORM[0]!=0)
+	return _PLATFORM;
+
+    struct utsname u;
+    int i;
+    if ( uname(&u) < 0 )
+	return "unknown";
+
+    strcat (_PLATFORM, u.sysname);
+    strcat (_PLATFORM, "-");
+    strcat (_PLATFORM, u.machine);
+    for (i=0; _PLATFORM[i]; i++)
+	_PLATFORM[i]=tolower(_PLATFORM[i]);
+    return _PLATFORM;
+}
+#else
+
+#ifndef PLATFORM
+#define PLATFORM "unknown"
+#endif
+
+const char *
+Py_GetPlatform(void)
+{
+	return PLATFORM;
+}
+#endif
